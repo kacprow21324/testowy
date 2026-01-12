8978613ac8c2439fb0032f34754f15f23d4f317a
kacprow21324 <kawoszczylo.work@gmail.com>
2026-01-12 10:20:21 +0100

    Stworzenie test1
    stworzenie test1 na branchu test1

--- test/src/App.js
+++ test/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
+import StronaTest1 from './StronaTest1';
 
 function App() {
   return (
@@ -8,9 +9,11 @@ function App() {
       <div className="App">
         <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
           <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
+          <Link to="/test1">Idź do Test 1</Link>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/test1" element={<StronaTest1 />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ test/src/StronaTest1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function StronaTest1() {
+  return (
+    <div style={{ backgroundColor: '#4feaff', padding: '20px' }}>
+      <h1>To jest STRONA TESTOWA 1</h1>
+      <p>Ten kod istnieje tylko na branchu test1.</p>
+    </div>
+  );
+}
+
+export default StronaTest1;
